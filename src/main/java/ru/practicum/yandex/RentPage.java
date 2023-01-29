1a10bf2f84e2ac80364cb109d5c39fc5d512d3e5
Kristina Suhoverhova <kri_smile92@mail.ru>
2023-01-30 01:07:55 +0300

    second commit

--- src/main/java/ru/practicum/yandex/RentPage.java
+++ src/main/java/ru/practicum/yandex/RentPage.java
@@ -7,19 +7,17 @@ public class RentPage {
     private final WebDriver driver;
     //Локатор для поля когда привезти самокат
     private final By deliveryDate = By.cssSelector("div.react-datepicker__input-container>input");
-
     // Локатор для треугольника в поле Срок аренды
     private final By rentTime = By.className("Dropdown-arrow");
-
     //Локатор для полей с указанием суток (в выпадающем списке после клика на треугольник)
     private final By rentDay = By.className("Dropdown-option");
-
     //Локатор для кнорки Заказть
     private final By orderScooterButton = By.xpath(".//div[@class='Order_Buttons__1xGrp']//button[text()='Заказать']");
     //Локатор для кнопки Да
     private final By getOrderYesButton = By.xpath(".//button[text()='Да']");
     // Информация подтверждения заказа
-    private final By orderInfo = By.className("Order_ModalHeader__3FDaJ");
+    private final By orderInfo = By.className("Order_Modal__YZ-d3");
+    private final By orderText = By.className("Order_ModalHeader__3FDaJ");
     public RentPage (WebDriver driver){
         this.driver=driver;
     }
@@ -31,22 +29,21 @@ public class RentPage {
     public void clickRentTime() {
         driver.findElement(rentTime).click();
     }
-
     public void clickRentTimeDays(int day) {
         driver.findElements(rentDay).get(day).click();
     }
-
     public void clickOrderButton() {
         driver.findElement(orderScooterButton).click();
     }
     public void clickOrderYesButton() {
         driver.findElement(getOrderYesButton).click();
     }
-
-    public String isOrderDisplayed() {
-        return driver.findElement(orderInfo).getText();
+    public boolean isOrderDisplayed() {
+        return driver.findElement(orderInfo).isDisplayed();
     }
-
+    public String getOrderText () {
+        return driver.findElement(orderText).getText();
+    }
     public void fillRentPage(String rentDate, int rentTimeDays) {
         this.setRentDate(rentDate);
         this.clickRentTime();
--- src/test/java/ru/yandex/practicum/TestOrderScooter.java
+++ src/test/java/ru/yandex/practicum/TestOrderScooter.java
@@ -1,6 +1,5 @@
 package ru.yandex.practicum;
 
-
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.After;
 import org.junit.Before;
@@ -19,12 +18,8 @@ import org.openqa.selenium.firefox.FirefoxOptions;
 import ru.practicum.yandex.MainPage;
 import ru.practicum.yandex.OrderPage;
 import ru.practicum.yandex.RentPage;
-
 import java.util.concurrent.TimeUnit;
-
-import static org.hamcrest.CoreMatchers.containsString;
-import static org.hamcrest.MatcherAssert.assertThat;
-
+import static org.junit.Assert.assertTrue;
 @RunWith(Parameterized.class)
 public class TestOrderScooter {
     WebDriver driver;
@@ -36,26 +31,23 @@ public class TestOrderScooter {
     private final int metroStation;
     private final String deliveryDate;
     private final int deliveryDay;
-
-    public TestOrderScooter(Boolean flag, String name, String surName, String adress, String pnoneNumber ,int metroStation,String deliveryDate,int deliveryDay) {
+    public TestOrderScooter(Boolean flag, String name, String surName, String adress, String pnoneNumber, int metroStation, String deliveryDate, int deliveryDay) {
         this.flag = flag;
         this.name = name;
         this.surName = surName;
         this.adress = adress;
         this.pnoneNumber = pnoneNumber;
-        this.metroStation=metroStation;
-        this.deliveryDate=deliveryDate;
-        this.deliveryDay=deliveryDay;
+        this.metroStation = metroStation;
+        this.deliveryDate = deliveryDate;
+        this.deliveryDay = deliveryDay;
     }
-
     @Parameterized.Parameters
     public static Object[][] getOrderData() {
         return new Object[][]{
-                {true,"Кристина", "Суховерхова", "Прошлякова 13", "89162733062", 1 ,"11.11.2023",3},
-                {false,"Максим", "Суховерхов", "Гагарина 14", "89162235899",2,"17.02.2023",4}
+                {true, "Кристина", "Суховерхова", "Прошлякова 13", "89162733062", 1, "11.11.2023", 3},
+                {false, "Максим", "Суховерхов", "Гагарина 14", "89162235899", 2, "17.02.2023", 4}
         };
     }
-
     @Before
     public void setUp() {
         WebDriverManager.chromedriver().setup();
@@ -67,35 +59,30 @@ public class TestOrderScooter {
         driver.get("https://qa-scooter.praktikum-services.ru/");
 
     }
+     /*@Before
+     public void setUp() {
+         WebDriverManager.firefoxdriver().setup();
+             FirefoxOptions options = new FirefoxOptions();
+             options.addArguments("--no-sandbox", "--disable-dev-shm-usage");
+             driver = new FirefoxDriver(options);
+         driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+         driver.manage().window().maximize();
+         driver.get("https://qa-scooter.praktikum-services.ru/");
 
-
-
-   /* @Before
-    public void setUp() {
-        WebDriverManager.firefoxdriver().setup();
-            FirefoxOptions options = new FirefoxOptions();
-            options.addArguments("--no-sandbox", "--disable-dev-shm-usage");
-            driver = new FirefoxDriver(options);
-        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-        driver.manage().window().maximize();
-        driver.get("https://qa-scooter.praktikum-services.ru/");
-
-    }*/
+     }*/
     @Test
     public void orderScooter() {
-            MainPage mainPage = new MainPage(driver);
-            mainPage.clickOrderButton(flag);
-            OrderPage orderPage = new OrderPage(driver);
-            orderPage.fillOrderPage(name, surName, adress,pnoneNumber, metroStation);
-            RentPage rentPage = new RentPage(driver);
-            rentPage.fillRentPage(deliveryDate, deliveryDay);
-            assertThat("текст с подтверждением заказа",
-                    rentPage.isOrderDisplayed(),
-                    containsString("Заказ оформлен"));
-        }
+        MainPage mainPage = new MainPage(driver);
+        mainPage.clickOrderButton(flag);
+        OrderPage orderPage = new OrderPage(driver);
+        orderPage.fillOrderPage(name, surName, adress, pnoneNumber, metroStation);
+        RentPage rentPage = new RentPage(driver);
+        rentPage.fillRentPage(deliveryDate, deliveryDay);
+        assertTrue(rentPage.isOrderDisplayed());
+    }
     @After
     public void tearDown() {
         driver.quit();
     }
-    }
+}
 
